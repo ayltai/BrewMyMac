66ca6f456ec8f715e15ef64132d99f322eb40e45
Alan Tai <ayltai@gmail.com>
2022-09-14 09:35:00 +0800

    Add GitHub workflows

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { GA4React, } from 'ga-4-react';
 
-export const handleError = (error : Error) => {
+export const handleError = (error : any) => {
     console.error(error);
 
     if (process.env.REACT_APP_GA_TAG) new GA4React(process.env.REACT_APP_GA_TAG).event('Error', JSON.stringify(error), 'Error');
